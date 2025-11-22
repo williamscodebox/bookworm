98ce9ad991f5e6f87f6b08bcf5c5eccd56fd76a0
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-22 14:10:31 -0500

    updated server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,4 +12,5 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
