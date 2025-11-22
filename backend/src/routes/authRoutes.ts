cc96a7308e6d45a45664912160623c4d29900b68
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-22 17:19:29 -0500

    updated authRoutes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,12 +3,12 @@ import type { Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/register", (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   // Handle user login
   res.send("User has been registered");
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   // Handle user login
   res.send("User logged in");
 });
