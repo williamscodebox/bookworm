29a9aa49670a25ee9c1f8fbc085ff9887ac9d0bf
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-22 21:43:46 -0500

    added User Model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import { Document, Model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  profileImage?: string;
+  comparePassword(userPassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+// hash password before saving user to db
+userSchema.pre<IUser>("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// compare password func
+userSchema.methods.comparePassword = async function (
+  this: IUser,
+  userPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+
+export default User;
