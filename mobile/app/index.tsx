f3e1d7884f92d47cbc94398c8dd4b17609704b50
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-25 16:48:04 -0500

    updated index

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
@@ -10,6 +11,11 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={{
+          uri: "https://res.cloudinary.com/dirm2bp53/image/upload/v1763951952/kpnncfde0xozzqiucbny.jpg",
+        }}
+      ></Image>
     </View>
   );
 }
