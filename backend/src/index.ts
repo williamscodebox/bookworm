0978afa70902910a8087e8cbb9db087d1316564e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-11-24 22:45:01 -0500

    added a cron job

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import type { Request, Response } from "express"; // 👈 use `type` keyword
 import "dotenv/config";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.ts";
 import bookRoutes from "./routes/bookRoutes.ts";
 import { connectDB } from "./lib/db.ts";
+import job from "./lib/cron.ts";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+job.start();
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
